fffb77e81514837489aa6d40c4dc69b8f0447283
Frank.Qiu <Frank.Qiu@fanruan.com>
2018-12-15 22:22:15 +0800

    修改定时时间

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const schedule = require('node-schedule');
 const bilibili = require('./src/scan/bilibili');
 const jd = require('./src/scan/jd');
 
-var j = schedule.scheduleJob('10 9 * * *', function() {
+var j = schedule.scheduleJob('30 30 12 * * *', function() {
   bilibili();
   jd();
 });
